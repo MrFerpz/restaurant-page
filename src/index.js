6a7094e96e804e873354903cfb658ebe4aea8811
Fred <leferpz@gmail.com>
2024-04-28 15:28:01 -0400

    Added basic background and set-up CSS loader

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log("butts");
+import "./style.css";
+import Background from "./images/yogbackground.png";
+
+const body = document.querySelector("body");
+
+const title = document.createElement("h1");
+title.setAttribute("id", "title");
+title.innerHTML = "Mo's Yoghurt";
+body.appendChild(title);
+
+const myBackground = new Image();
+myBackground.src = Background;
+myBackground.setAttribute("id", "background");
+body.appendChild(myBackground);
